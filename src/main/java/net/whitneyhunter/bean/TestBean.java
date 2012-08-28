7a3aedc2ac13b0a37d003010d4c0ed1277fe36e3
Whitney Hunter <whitney@hunterfam.com>
2012-08-27 20:42:29 -0700

    Added spring instrument library

--- src/main/java/net/whitneyhunter/bean/TestBean.java
+++ src/main/java/net/whitneyhunter/bean/TestBean.java
@@ -5,6 +5,12 @@ import net.whitneyhunter.service.Injectable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
+/**
+ * This bean will be constructed by the new operator; however, it needs to have
+ * a service injected by Spring.
+ * 
+ * The @Configurable annotation calls Spring to try to wire this class.
+ */
 @Configurable
 public class TestBean {
 
